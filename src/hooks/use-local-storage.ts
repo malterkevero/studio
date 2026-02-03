7d2cf38f22395320a720621227f51b6631d21451
Pokoraczki Péter <PokoraczkiP456@gmail.com>
2026-02-03 10:13:37 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -20,7 +20,7 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, React.Dispatch<Re
                 setStoredValue(initialValue);
             }
         }
-    }, [isMounted, key, initialValue]);
+    }, [isMounted, key]);
 
     const setValue: React.Dispatch<React.SetStateAction<T>> = (value) => {
         if (typeof window === 'undefined') {
